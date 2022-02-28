66ac050f36d4c259cf76355c4ebe322ce7991b62
debilsalakhov <bigmoyka@gmail.com>
2022-02-28 16:40:09 +0300

    v1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Вариант 1 - самый простой чат бот, просто отзывается
+# Телеграмм бот версии 2.0 - имеет меню
 
 import telebot  # pyTelegramBotAPI	4.3.1
 from telebot import types
@@ -11,10 +11,13 @@ def start(message, res=False):
     chat_id = message.chat.id
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Главное меню')
+    btn2 = types.KeyboardButton('Помощь')
+    markup.add(btn1, btn2)
 
     bot.send_message(chat_id,
                      text="Йоу здорова, {0.first_name}! Я тестовый бот для курса программирования на языке Питон! "
-                          "Пока еще я ничего не умею, но скоро буду.".format(message.from_user))
+                          "Пока еще я ничего не умею, но скоро буду.".format(message.from_user), reply_markup=markup)
 
 
 # -----------------------------------------------------------------------
@@ -23,8 +26,39 @@ def start(message, res=False):
 def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
-    bot.send_message(chat_id, text="Понял тебя. Ты написал: " + ms_text)
 
+    if ms_text == 'Главное меню' or ms_text == 'Вернуться в главное меню':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Развлечения')
+        btn2 = types.KeyboardButton('Камера')
+        btn3 = types.KeyboardButton('Управление')
+        back = types.KeyboardButton('Помощь')
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(chat_id, text='Вы в главном меню', reply_markup=markup)
+    elif ms_text == 'Развлечения':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Прислать собаку')
+        btn2 = types.KeyboardButton('Прислать анекдот')
+        back = types.KeyboardButton('Вернуться в главное меню')
+        markup.add(btn1, btn2, back)
+        bot.send_message(chat_id, text='Развлечения', reply_markup=markup)
+    elif ms_text == '/dog' or ms_text == 'Прислать собаку':
+        bot.send_message(chat_id, text='Еще не готово...')
+    elif ms_text == 'Прислать анекдот':
+        bot.send_message(chat_id, text='Еще не готово...')
+    elif ms_text == 'Камера':
+        bot.send_message(chat_id, text='Еще не готово...')
+    elif ms_text == 'Управление':
+        bot.send_message(chat_id, text='Еще не готово...')
+    elif ms_text == 'Помощь' or ms_text == '/help':
+        bot.send_message(chat_id, 'Автор: Салахов Шамиль')
+        key1 = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='Напишите автору', url='https://t.me/debilsalakhov')
+        key1.add(btn1)
+        img = open('chad.jpg', 'rb')
+        bot.send_photo(message.chat.id, img, reply_markup=key1)
+    else:
+        bot.send_message(chat_id, text='Я тебя слышу!!! Ваше сообщение: ' + ms_text)
 
 # -----------------------------------------------------------------------
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
